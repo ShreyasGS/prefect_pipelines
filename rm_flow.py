6fa2cf9604a1e9e4084ab6cb5dd2904dfacc59ef
Shreyas <ss4shreyas@gmail.com>
2025-08-18 15:45:20 +0200

    fix cred string

--- rm_flow.py
+++ rm_flow.py
@@ -5,20 +5,19 @@ import os
 
 @flow(log_prints=True)
 def main(db_name: str = "dlt_test"):
-    # GitHub PAT for your dlt REST source
-    # (dlt.secrets["sources.access_token"] -> env var SOURCES__ACCESS_TOKEN)
-    os.environ["SOURCES__ACCESS_TOKEN"] = GitHubCredentials.load("github-pat").token
-
-    # MotherDuck token from your block
-    md_token = Secret.load("motherduck-token").value
-    
+    # MotherDuck token (Prefect Secret -> str)
+    md_token = Secret.load("motherduck-token").get()          # <- use .get(), not .value
+    os.environ["MOTHERDUCK_TOKEN"] = md_token
     os.environ["DESTINATION__MOTHERDUCK__CREDENTIALS__PASSWORD"] = md_token
-
-    # Tell dlt which MotherDuck database to use
     os.environ["DESTINATION__MOTHERDUCK__CREDENTIALS__DATABASE"] = db_name
+
+    # GitHub PAT (GitHubCredentials.token is SecretStr -> use .get_secret_value())
+    pat = GitHubCredentials.load("github-pat").token.get_secret_value()
+    os.environ["SOURCES__ACCESS_TOKEN"] = pat  # dlt.secrets["sources.access_token"]
 
     import github_pipeline
     return github_pipeline.run_pipeline()
+
 
 if __name__ == "__main__":
     main()
