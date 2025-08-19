f267a79e485fc88a16bbf408721d27c34666e70f
Shreyas <ss4shreyas@gmail.com>
2025-08-19 17:40:45 +0200

    second pipeline

---
+++ github_mlt_pipeline.py
@@ -0,0 +1,28 @@
+import dlt
+from dlt.sources.rest_api import RESTAPIConfig, rest_api_source
+
+config: RESTAPIConfig = {
+    "client": {
+        "base_url": "https://api.github.com",
+        "auth": {
+            "token": dlt.secrets["sources.access_token"], 
+        },
+        "paginator": "header_link" 
+    },
+    "resources": [  
+        {
+            "name": "repos",
+            "endpoint": {
+                "path": "orgs/dlt-hub/repos"
+            },
+        },
+        {
+          "name": "contributors",
+          "endpoint": {
+            "path": "repos/dlt-hub/dlt/contributors",
+          },
+        },
+    ],
+}
+
+github_source = rest_api_source(config)
---
+++ rm_flow_mlt.py
@@ -0,0 +1,44 @@
+from prefect import flow, task
+from prefect.task_runners import ConcurrentTaskRunner
+from prefect.blocks.system import Secret
+import os
+import dlt
+from prefect_github import GitHubCredentials
+from prefect.futures import wait
+from prefect.task_runners import ThreadPoolTaskRunner
+import github_pipeline  # has github_source + run_pipeline helpers
+
+def _set_env(md_db: str):
+    # GitHub PAT (GitHubCredentials.token is SecretStr -> use .get_secret_value())
+    pat = GitHubCredentials.load("github-pat").token.get_secret_value()
+    os.environ["SOURCES__ACCESS_TOKEN"] = pat  # dlt.secrets["sources.access_token"]
+    # MotherDuck auth + DB name
+    md_token = Secret.load("motherduck-token").get()
+    os.environ["MOTHERDUCK_TOKEN"] = md_token
+    os.environ["DESTINATION__MOTHERDUCK__CREDENTIALS__PASSWORD"] = md_token
+    os.environ["DESTINATION__MOTHERDUCK__CREDENTIALS__DATABASE"] = md_db
+
+@task(retries=2, log_prints=True)
+def run_resource(resource_name: str, md_db: str):
+    _set_env(md_db)
+    # pick just one resource from your dlt source
+    src = github_pipeline.github_source.with_resources(resource_name)
+    # unique pipeline per resource avoids dlt state clashes
+    pipe = dlt.pipeline(
+        pipeline_name=f"rest_api_github__{resource_name}",
+        destination="motherduck",
+        dataset_name="rest_api_data_mlt",
+        progress="log",
+    )
+    info = pipe.run(src)
+    print(f"{resource_name} -> {info}")
+    return info
+
+@flow(task_runner=ThreadPoolTaskRunner(max_workers=2), log_prints=True)
+def main(md_db: str = "dlt_test"):
+    a = run_resource.submit("repos", md_db)
+    b = run_resource.submit("contributors", md_db)
+    return a.result(), b.result()
+
+if __name__ == "__main__":
+    main()
